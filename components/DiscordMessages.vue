e1b473a31397f9f1abb7346a4cbf680cc50a4758
Gavin Mogan <git@gavinmogan.com>
2022-01-30 20:31:13 -0800

    feat: image approval is now prettier

--- components/DiscordMessages.vue
+++ components/DiscordMessages.vue
@@ -2,15 +2,18 @@
   <div class="max-w-4xl mx-auto sm:px-6 lg:px-8 grow">
     <div class="mt-8 bg-white overflow-hidden shadow sm:rounded-lg p-6 w-full">
       <div v-if="messages != null">
-        <div v-for="message in messages" :key="message.id" class="p-2">
-          <img :src="message.attachments[0]">
-          <b-form-checkbox v-model="checked[message.id]" name="check-button" switch>
-            Switch Checkbox <b>(Checked: {{ checked[message.id] }})</b>
-          </b-form-checkbox>
-          <blockquote>{{ message.content }}</blockquote>
-          <dd>By: {{ message.author }}</dd>
-          <dd>At: {{ formatTime(message.createdTimestamp) }}</dd>
-        </div>
+        <b-row>
+          <b-col v-for="msg in messages" :key="msg.id" :style="boxStyles(msg.id)" class="m-2 p-5" @click="toggle(msg.id)">
+            <img :src="msg.attachments[0]" style="height: 10em;">
+            <blockquote>{{ msg.content }}</blockquote>
+            <dd>By: {{ msg.author }}</dd>
+            <dd>At: {{ formatTime(msg.createdTimestamp) }}</dd>
+            <b-form-checkbox v-model="checked[msg.id]" name="check-button" switch>
+              Approved
+            </b-form-checkbox>
+          </b-col>
+        </b-row>
+        </b-row>
       </div>
     </div>
   </div>
@@ -33,9 +36,23 @@ export default {
       messages: 'messages'
     })
   },
+
   methods: {
+    boxStyles (id) {
+      return {
+        userSelect: 'none',
+        width: '20em',
+        border: `2px solid ${this.checked[id] ? 'green' : 'red'}`,
+        backgroundColor: this.checked[id] ? '#ACD1AF' : '#F47174',
+        borderRadius: '10%',
+        height: '20em'
+      }
+    },
+    toggle (id) {
+      this.checked = { ...this.checked, [id]: !this.checked[id] }
+    },
     formatTime (timestamp) {
-      return new Date(timestamp)
+      return new Date(timestamp).toLocaleString()
     },
     async selectChannel (channelId) {
       this.socket = this.$nuxtSocket({ persist: 'defaultLabel' })
