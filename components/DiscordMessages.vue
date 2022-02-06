dd8d314961c47b1c2dab252eb14cafb02a5023a3
Gavin Mogan <git@gavinmogan.com>
2022-02-05 22:26:54 -0800

    pretty up the messages sections

--- components/DiscordMessages.vue
+++ components/DiscordMessages.vue
@@ -1,14 +1,14 @@
 <template>
   <div v-if="messages != null">
     <b-row cols="1" cols-sm="2" cols-md="4" cols-lg="6">
-      <b-col v-for="msg in messages" :key="msg.id" :style="boxStyles(msg.id)" class="m-2 p-5" @click="toggle(msg.id)">
-        <img :src="msg.attachment" style="height: 10em;">
-        <blockquote>{{ msg.content }}</blockquote>
-        <dd>By: {{ msg.author }}</dd>
-        <dd>At: {{ formatTime(msg.createdTimestamp) }}</dd>
+      <b-col v-for="msg in messages" :key="msg.id" :style="boxStyles(msg.id)" class="m-2 p-3" @click="toggle(msg.id)">
+        <div :style="imgStyles(msg.attachment)" />
         <b-form-checkbox v-model="checked[msg.id]" name="check-button" switch>
           Approved
         </b-form-checkbox>
+        <blockquote>{{ msg.content }}</blockquote>
+        <dd>By: {{ msg.author }}</dd>
+        <dd>At: {{ formatTime(msg.createdTimestamp) }}</dd>
       </b-col>
     </b-row>
   </div>
@@ -39,9 +39,21 @@ export default {
         border: `2px solid ${this.checked[id] ? 'green' : 'red'}`,
         backgroundColor: this.checked[id] ? '#ACD1AF' : '#F47174',
         borderRadius: '10%',
-        height: '20em'
+        height: '24em',
+        width: '100%'
       }
     },
+
+    imgStyles (url) {
+      return {
+        height: '14em',
+        backgroundImage: `url(${url})`,
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'contain',
+        backgroundPosition: 'center'
+      }
+    },
+
     toggle (id) {
       this.checked = { ...this.checked, [id]: !this.checked[id] }
     },
