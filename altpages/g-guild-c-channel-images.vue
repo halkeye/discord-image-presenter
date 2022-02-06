18286afcdedd5c796b9a8810f37d1995be0f815d
Gavin Mogan <git@gavinmogan.com>
2022-02-06 13:05:31 -0800

    Switch from using carousel which was having issues with new images to custom
    Used basic 3d flipping from https://www.w3schools.com/howto/howto_css_flip_card.asp
    
    Thanks so much to @nfg for helping with the "math" on this one
    We now keep back and front tracked so we can change it before flipping

--- altpages/g-guild-c-channel-images.vue
+++ altpages/g-guild-c-channel-images.vue
@@ -1,51 +1,80 @@
 <template>
-  <VueSlickCarousel
-    v-if="messages && messages.length"
-    ref="carousel"
-    :arrows="true"
-    :dots="false"
-    :adaptive-height="true"
-    :autoplay="true"
-    :center-mode="true"
-    :infinite="true"
-  >
-    <div v-for="message in messages" :key="message.id" :style="styleFor(message)" class="att-container vh-100" />
-  </VueSlickCarousel>
+  <div v-if="frontImage" :class="'flip-card' + flippedClassName">
+    <div class="flip-card-inner">
+      <div class="flip-card-front">
+        <img :src="frontImage.attachment">
+      </div>
+      <div v-if="backImage" class="flip-card-back">
+        <img :src="backImage.attachment">
+      </div>
+    </div>
+  </div>
   <b-spinner v-else label="Loading" />
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
-import VueSlickCarousel from 'vue-slick-carousel'
-import 'vue-slick-carousel/dist/vue-slick-carousel.css'
-// optional style for arrows & dots
-import 'vue-slick-carousel/dist/vue-slick-carousel-theme.css'
 
 export default {
   name: 'ChannelImagesPage',
   auth: true,
-  components: {
-    VueSlickCarousel
-  },
   layout: 'empty',
+  data () {
+    return {
+      messages: [],
+      messageIdx: 0,
+      frontImage: null,
+      backImage: null,
+      flipped: false,
+      counterInterval: null
+    }
+  },
   computed: {
-    ...mapGetters({ messages: 'authorizedMessages' })
+    ...mapGetters({ authorizedMessages: 'authorizedMessages' }),
+    flippedClassName () {
+      return this.flipped ? ' flipped' : ' notflipped'
+    }
   },
   watch: {
-    messages () {
-      if (this.$refs.carousel) {
-        this.$refs.carousel.play()
+    authorizedMessages () {
+      // if theres no (cached) messages, and some get authorized, copy them over
+      if (!this.messages.length) {
+        this.messages = this.authorizedMessages
       }
     }
   },
+  created () {
+    this.counterInterval = setInterval(this.showNextImage.bind(this), 3000)
+
+    // make a copy
+    this.messages = this.authorizedMessages
+    // preset to first message
+    this.frontImage = this.messages[0]
+  },
+  destroyed () {
+    clearInterval(this.counterInterval)
+    clearInterval(this.flippedInterval)
+  },
   methods: {
-    styleFor (msg) {
-      return {
-        backgroundImage: `url(${msg.attachment})`,
-        backgroundRepeat: 'no-repeat',
-        backgroundSize: 'contain',
-        backgroundPosition: 'center'
+    showNextImage () {
+      // if next image is outside bounds, copy
+      if (this.messageIdx + 1 >= this.messages.length) {
+        this.messages = this.authorizedMessages
+        this.messageIdx = 0
+      } else {
+        this.messageIdx += 1
       }
+
+      const nextImageToShow = this.messages[this.messageIdx]
+
+      if (this.flipped) {
+        // back is currently shown
+        this.frontImage = nextImageToShow
+      } else {
+        // front is currently shown
+        this.backImage = nextImageToShow
+      }
+      this.flipped = !this.flipped
     }
   }
 }
@@ -55,10 +84,44 @@ export default {
 body {
   overflow: hidden
 }
-
-/*
-.slick-slide:not(.slick-active) .att-container {
-  display: none !important;
+.flip-card {
+  height: 100vh !important;
+  background-color: transparent;
+  border: 1px solid #f1f1f1;
+  perspective: 1000px;
 }
-*/
+
+.flip-card img {
+  height: 100%; /* remove if dont want to stretch */
+  max-height: 100vh !important;
+  max-width: 100vw !important;
+}
+
+.flip-card .flip-card-inner {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  text-align: center;
+  transition: transform 0.8s;
+  transform-style: preserve-3d;
+}
+
+.flip-card.flipped .flip-card-inner {
+  transform: rotateY(180deg);
+}
+
+.flip-card .flip-card-front, .flip-card .flip-card-back {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  -webkit-backface-visibility: hidden;
+  backface-visibility: hidden;
+}
+
+.flip-card .flip-card-front {
+}
+
+.flip-card .flip-card-back {
+  transform: rotateY(180deg);
+}
 </style>
